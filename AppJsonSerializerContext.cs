5ee8c8793fc1fb9722be3b5623c158a5be0dad9e
Oschangkai <oschangkai@kaiyeee.tw>
2024-01-08 23:36:49 +0800

    Update: remove default endpoints

--- AppJsonSerializerContext.cs
+++ AppJsonSerializerContext.cs
@@ -1,7 +1,6 @@
 using System.Text.Json.Serialization;
 
 [JsonSerializable(typeof(SystemHealth))]
-[JsonSerializable(typeof(Todo[]))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
 
